76a0b058f250a86af74b399d51f680dbd26685bf
Daan van Hekken <90179778+daanvhekken@users.noreply.github.com>
2023-07-09 13:54:43 +0200

    display list of items fetch via cloudfunction

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+function fetchTodos() {
+  fetch('https://us-central1-cco-project-1b0ee.cloudfunctions.net/getTodos')
+    .then(response => response.json())
+    .then(data => {
+      const todoListContainer = document.getElementById('todoList');
+      
+      // Clear existing todo items
+      todoListContainer.innerHTML = '';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAB_BpmvbYMTWfxW3IHiX-c0gJf233dwu0",
-  authDomain: "cco-project-1b0ee.firebaseapp.com",
-  projectId: "cco-project-1b0ee",
-  storageBucket: "cco-project-1b0ee.appspot.com",
-  messagingSenderId: "311063493349",
-  appId: "1:311063493349:web:f547a193b7066db63cf20c"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+      // Iterate over the todo items and create list items
+      data.forEach(todo => {
+        const listItem = document.createElement('li');
+        listItem.textContent = todo.title;
+        todoListContainer.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+fetchTodos();
